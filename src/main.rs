574a5236b8445ae7def54cde8b54e68df14c59aa
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2024-01-01 15:14:50 -0300

    feat: initial movement (WIP)

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,30 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
+use piston::Key;
+use piston::{Button, PressEvent};
 
 pub struct App {
     gl: GlGraphics,
     rotation: f64,
+    x: f64,
+    y: f64,
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 50.0);
         let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let x = self.x;
+        let y = self.y;
 
         self.gl.draw(args.viewport(), |c, gl| {
-            clear(GREEN, gl);
+            clear(BLACK, gl);
 
             let transform = c
                 .transform
@@ -34,19 +39,31 @@ impl App {
                 .rot_rad(rotation)
                 .trans(-25.0, -25.0);
 
-            rectangle(RED, square, transform, gl);
+            rectangle(WHITE, square, transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         self.rotation += 2.0 * args.dt;
     }
+
+    fn move_player(&mut self, button: &Button) {
+        if let Button::Keyboard(key) = *button {
+            match key {
+                Key::Up => self.y -= 10.0,
+                Key::Left => self.x -= 10.0,
+                Key::Right => self.x += 10.0,
+                Key::Down => self.y += 10.0,
+                _ => (),
+            }
+        }
+    }
 }
 
 fn main() {
     let opengl = OpenGL::V3_2;
 
-    let mut window: Window = WindowSettings::new("game-test", [200, 200])
+    let mut window: Window = WindowSettings::new("game-test", [860, 780])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -55,9 +72,14 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0,
+        x: 300.0,
+        y: 30.0,
     };
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.press_args() {
+            app.move_player(&args);
+        }
         if let Some(args) = e.render_args() {
             app.render(&args);
         }
